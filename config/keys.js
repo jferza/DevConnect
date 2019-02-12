940e9db567a64cb9c31329b08a8ab4bdee83a64d
jferza <jonasferza@gmail.com>
2019-02-12 18:55:40 +0000

    Initial server with routes

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://jdb:jdb@devconnector-shard-00-00-5pbca.mongodb.net:27017,devconnector-shard-00-01-5pbca.mongodb.net:27017,devconnector-shard-00-02-5pbca.mongodb.net:27017/test?ssl=true&replicaSet=devconnector-shard-0&authSource=admin&retryWrites=true"
+};
